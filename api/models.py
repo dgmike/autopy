698f0a6d93cc18c6fdecbd181d93723780d5cccf
Michael Granados <michaelgranados@gmail.com>
2016-08-22 08:17:54 -0300

    adding created_at and updated_at columns
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- api/models.py
+++ api/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class VehicleType(models.Model):
   name = models.CharField(max_length=100)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
   def __str__(self):
     return self.name
@@ -10,6 +12,8 @@ class VehicleType(models.Model):
     return {
       "id": self.id,
       "name": self.name,
+      "created_at": self.created_at,
+      "updated_at": self.updated_at,
       "_links": {
         "self": self.self_url()
       }
