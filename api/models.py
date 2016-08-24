9586b232208fbab129d6a6e51e27cf7e7af3777d
Michael Granados <michaelgranados@gmail.com>
2016-08-23 22:37:57 -0300

    creating abstract model
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- api/models.py
+++ api/models.py
@@ -1,11 +1,28 @@
 from django.db import models
 import datetime
 
-class VehicleType(models.Model):
-  name = models.CharField(max_length=100, db_index=True)
+class AbstractBaseModel(models.Model):
   created_at = models.DateTimeField(auto_now_add=True, db_index=True)
   updated_at = models.DateTimeField(auto_now=True, db_index=True)
   deleted_at = models.DateTimeField(null=True, db_index=True)
+
+  class Meta:
+    abstract = True
+
+  def has_some(self, relationship):
+    relationship_set = "%s_set" % relationship
+    count = getattr(self, relationship_set).filter(deleted_at__isnull=True).count()
+    return count > 0
+
+  def delete(self, *args, **kwargs):
+    self.deleted_at = datetime.datetime.now()
+    self.save()
+
+  def drop(self):
+    super(__class__, self).delete()
+
+class VehicleType(AbstractBaseModel):
+  name = models.CharField(max_length=100, db_index=True)
 
   def __str__(self):
     return self.name
@@ -24,22 +41,6 @@ class VehicleType(models.Model):
   def self_url(self):
     return "/api/vehicle-types/%s" % self.id
 
-  def has_some(self, relationship):
-    relationship_set = "%s_set" % relationship
-    count = getattr(self, relationship_set).filter(deleted_at__isnull=True).count()
-    return count > 0
-
-  def delete(self, *args, **kwargs):
-    self.deleted_at = datetime.datetime.now()
-    self.save()
-
-  def drop(self):
-    super(__class__, self).delete()
-
-class Manufacturer(models.Model):
+class Manufacturer(AbstractBaseModel):
   name = models.CharField(max_length=100, db_index=True)
   vehicle_type = models.ForeignKey('VehicleType', on_delete=models.PROTECT, db_index=True)
-
-  created_at = models.DateTimeField(auto_now_add=True, db_index=True)
-  updated_at = models.DateTimeField(auto_now=True, db_index=True)
-  deleted_at = models.DateTimeField(null=True, db_index=True)
